a02d3e5331e4600c01e84d8acbc3c3c31381350d
Alex Gregory <alex.gregory@made.com>
2022-06-24 00:49:31 +0100

    changed entrypoint

--- entry.sh
+++ entry.sh
@@ -10,9 +10,14 @@ export TERM=xterm-256color
 DATE=`/bin/date +%Y-%m-%d-%H:%M:%S`
 
 # Do we need to do this whole `pipeline` thing? What is the benefit of this?
-if [ "$1" = pipeline ]; then
-    echo "Starting "$2" pipeline."
-    /venv/bin/python src/$2/main.py
-    echo "Finished "$2" pipeline."
-    exit $?
-fi
+# if [ "$1" = pipeline ]; then
+#     echo "Starting "$2" pipeline."
+#     /venv/bin/python src/$2/main.py
+#     echo "Finished "$2" pipeline."
+#     exit $?
+# fi
+
+echo "Starting "$1" pipeline."
+/venv/bin/python src/$1/main.py
+echo "Finished "$1" pipeline."
+exit $?
